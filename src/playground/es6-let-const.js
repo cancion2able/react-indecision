57d7c26f90218941a8cf25f6aeed9e3a1759d7f1
Stanislav_Ovsyankin <Stanislav_Ovsyankin@epam.com>
2017-10-18 21:54:51 +0600

    Set up Babel

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,17 +1,15 @@
-'use strict';
-
 var nameVar = 'Andrew';
 var nameVar = 'Mike';
 console.log('nameVar', nameVar);
 
-var nameLet = 'Jen';
+let nameLet = 'Jen';
 console.log('nameLet', nameLet);
 
-var nameConst = 'Frank';
+const nameConst = 'Frank';
 console.log('nameConst', nameConst);
 
 var fullName = 'Andrew Mead';
-var firstName = void 0;
+let firstName;
 
 if (fullName) {
   firstName = fullName.split(' ')[0];
